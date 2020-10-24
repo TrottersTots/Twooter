7b3583bf1328e839581ac8700cea589f2a923307
Nathan Inbar <nathaninbar@gmail.com>
2020-10-24 13:46:55 -0700

    〽 added react routers
    added react-router via 'npm i react-router-dom'

--- main/twooter-app/src/Components/Sidebar.js
+++ main/twooter-app/src/Components/Sidebar.js
@@ -4,7 +4,7 @@ import SidebarOption from './SidebarOption';
 import TwootModal from './TwootModal';
 import UserProfile from "./UserProfile";
 import { Button } from "@material-ui/core";
-
+import {Route} from "react-router-dom";
 // Icons
 import TwitterIcon from '@material-ui/icons/Twitter';
 import HomeIcon from '@material-ui/icons/Home';
