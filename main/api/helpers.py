22207c258006eb148659caebaef37c239e505d81
Nathan Inbar <nathaninbar@gmail.com>
2020-11-05 15:36:31 -0800

    🌯 fixed avatars on all posts, profile css

--- main/api/helpers.py
+++ main/api/helpers.py
@@ -6,7 +6,8 @@ def appendAvatar(q, session):
     avatar_path = f"../twooter-app/public/avatars/{session['hashed_id']}.jpg"
     if(os.path.exists(os.path.join(current_dir, avatar_path))):  #if there is a custom avatar for this user
         #set an avatar attribute in the JSON to its dir
-        q[0].update({'avatar':session['hashed_id']})
-        
+        for query in q:
+            query.update({'avatar':session['hashed_id']})
+
 def query_to_dict (query):
     return [dict(row) for row in query.fetchall()]
